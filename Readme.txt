6d92afd7a4ce14b208ba2ea7072579b83a2b1619
Vamsikrishna Vuyyuru <vuyyuru.vamsikrishna@gmail.com>
2018-07-21 00:36:21 +0530

    Code for Google OAuth and Mongodb storage has been implemented

---
+++ Readme.txt
@@ -0,0 +1,23 @@
+Passport.js
+
+Node.js
+
+Express 
+
+Mongoose
+
+Mongo db
+
+React js
+
+Nodemon
+
+NPM
+
+
+
+
+database user 
+
+vamsi
+pwd: password1
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(
+  cookieSession({
+	  maxAge:  30 * 24 * 60 * 60 * 1000,
+	  keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*app.get('/', (req, res) => {
     res.send({hi: 'there'});
-});
+});*/
+//client id
+//246143129001-nr8blkfvrr0vlqf0vf3memf0tvsh4j0j.apps.googleusercontent.com
+
+require('./routes/authRoutes')(app);
+
+
+
 
 app.listen(process.env.PORT || 5000)
