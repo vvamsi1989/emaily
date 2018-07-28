a8f2284032ee878dfb91c2456cfc11195b4fea2f
Vamsikrishna Vuyyuru <vuyyuru.vamsikrishna@gmail.com>
2018-07-28 17:21:22 +0530

    changes made to server index.js

--- index.js
+++ index.js
@@ -5,38 +5,40 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
+
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(
   cookieSession({
-	  maxAge:  30 * 24 * 60 * 60 * 1000,
-	  keys: [keys.cookieKey]
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-/*app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});*/
-//client id
-//246143129001-nr8blkfvrr0vlqf0vf3memf0tvsh4j0j.apps.googleusercontent.com
-
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-	
-	app.use(express.static('client/build'));
-	
-	const path = require('path');
-	app.get('*',(req,res) => {
-		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-		
-	});
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-app.listen(process.env.PORT || 5000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
