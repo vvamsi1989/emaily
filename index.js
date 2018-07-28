a01d844e7da6c240e49bb92ee455722ad0ffab8b
Vamsikrishna Vuyyuru <vuyyuru.vamsikrishna@gmail.com>
2018-07-28 17:15:35 +0530

    Added billing and client side react-redux code

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
   cookieSession({
 	  maxAge:  30 * 24 * 60 * 60 * 1000,
@@ -25,8 +26,17 @@ app.use(passport.session());
 //246143129001-nr8blkfvrr0vlqf0vf3memf0tvsh4j0j.apps.googleusercontent.com
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
-
-
+if(process.env.NODE_ENV === 'production'){
+	
+	app.use(express.static('client/build'));
+	
+	const path = require('path');
+	app.get('*',(req,res) => {
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+		
+	});
+}
 
 app.listen(process.env.PORT || 5000)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,16 +6,22 @@ module.exports = app => {
     passport.authenticate('google', {
       scope: ['profile', 'email']
     })
-);
+  );
 
-    app.get('/auth/google/callback',passport.authenticate('google'));
-	app.get('/api/logout',(req,res) => {
-	req.logout();
-	res.send(req.user);
-});
-	app.get('/api/current_user',(req,res) => {
-		res.send(req.user);  
-	});
-};	
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
-	
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
